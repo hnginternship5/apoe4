2f360788f463ba299fbeba1d502395315e828010
Ahmzyjazzy <olanrewajuahmed095@yahoo.com>
2019-04-17 15:52:43 +0100

    refactor/merge PR from seyi and refactor code

--- src/components/about/TeamSection.vue
+++ src/components/about/TeamSection.vue
@@ -1,17 +1,17 @@
 <template>
-    <div class="container-fluid">
+    <div class="container-fluid" id="team">
         <div class="container">
-            <!-- ALL COMPONENT MARKUP GOES HERE-->
-            <h1>Meet the Team</h1>
+          <!-- ALL COMPONENT MARKUP GOES HERE-->
+          <h1>Meet the Team</h1>
           <h3>The team ready and waiting to talk about the new APoe4 App and subsequent ideas.</h3>
           <div id="img-area">
-	          <span><img src="@/assets/img/team/letoya2.jpg" class="rounded-circle"><h4>Osato Letoya Joy</h4><p>Team Lead</p></span>
-	            <span><img src="@/assets/img/team/s3.jpg" class="rounded-circle"><h4>Seyi Imanah</h4><p>Front-End</p></span>
-	            <span><img src="@/assets/img/team/ahmzyjazzy.jpg" class="rounded-circle"><h4>Ahmed Olarenwaju</h4><p>Front-End</p></span>
-	            <span><img src="@/assets/img/team/john.jpg" class="rounded-circle"><h4>John Ademoye</h4><p>Front-End</p></span>
-	            <span><img src="@/assets/img/team/zazah.jpg" class="rounded-circle"><h4>Abdulhakeem Abdulazeez</h4><p>Front-End</p></span>
-	            <span><img src="@/assets/img/team/jeff.jpg" class="rounded-circle"><h4>Jeffrey Ogah</h4><p>Developer</p></span>
-        </div>
+            <span><img src="@/assets/img/team/letoya2.jpg" class="rounded-circle"><h4>Osato Letoya Joy</h4><p>Team Lead</p></span>
+            <span><img src="@/assets/img/team/s3.jpg" class="rounded-circle"><h4>Seyi Imanah</h4><p>Front-End</p></span>
+            <span><img src="@/assets/img/team/ahmzyjazzy.jpg" class="rounded-circle"><h4>Ahmed Olarenwaju</h4><p>Front-End</p></span>
+            <span><img src="@/assets/img/team/john.jpg" class="rounded-circle"><h4>John Ademoye</h4><p>Front-End</p></span>
+            <span><img src="@/assets/img/team/zazah.jpg" class="rounded-circle"><h4>Abdulhakeem Abdulazeez</h4><p>Front-End</p></span>
+            <span><img src="@/assets/img/team/jeff.jpg" class="rounded-circle"><h4>Jeffrey Ogah</h4><p>Developer</p></span>
+         </div>
         </div>
     </div>
 </template>
@@ -20,13 +20,8 @@
 	@import url('https://fonts.googleapis.com/css?family=Poppins');
 	@import url('https://fonts.googleapis.com/css?family=Nunito+Sans|Poppins');
 
-	html, body {
-  margin: 0;
-  padding: 0;
-}
-
-h1 {
-margin-top: 100px;
+#team h1 {
+  margin-top: 100px;
   font-family: Poppins;
   font-style: normal;
   font-weight: bold;
@@ -37,7 +32,7 @@ margin-top: 100px;
 
 }
 
-h3 {
+#team h3 {
   font-family: Nunito Sans;
   font-style: normal;
   font-weight: 600;
@@ -46,18 +41,25 @@ h3 {
   line-height: normal;
   color: #000000;
 }
-img {
+#team img {
   width: 217px;
   height: 217px;
   border-radius: 100%;
   margin-top: 50px;
 }
-#img-area span {
+
+#team #img-area {
+  content: '';
+  clear: both;
+}
+
+#team #img-area span {
 	display: inline;
 	float: left;
-	margin-left: 100px;
+  margin-left: 100px;
 }
-#img-area span h4, #img-area span p {
+#team #img-area span h4, 
+#team #img-area span p {
 	text-align: center;
 	font-family: Nunito Sans;
 	font-style: normal;
@@ -68,11 +70,11 @@ img {
 	color: #000000;
 	margin-top: 10px;
 }
-#img-area span p {
+#team #img-area span p {
 	font-size: 16px;
 	font-weight: 600;
 }
-.img-area:after {
+#team .img-area:after {
     content: "";
     display: block;
     clear: both;
