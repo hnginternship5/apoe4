31d0c51f40a45aad22b3aa97bd1dbbc6d8d853d7
Ayooluwa Oyewo <ayooluwaoyewo@gmail.com>
2019-04-19 07:44:22 +0100

    updated the contact page address look

--- src/components/contact/AddressSection.vue
+++ src/components/contact/AddressSection.vue
@@ -10,14 +10,22 @@
             <div class="col-md-2 icon text-center">
                 <img class="contact-image" src="../../assets/img/phonecall.png" alt="call apoe4">
                 <h5 class="contact-heading mt-5">CALL US</h5>
+                <div class="link">
                 <a href="tel:+2348012334590" class="contact-link">+2348012334590</a>
+                <br/>
                 <a href="tel:+2348012345678" class="contact-link">+2348012345678</a>
+                </div>
+                
             </div>
             <div class="col-md-2 icon text-center">
-                <img class="contact-image " src="../../assets/img/mail.png" alt="mail apoe4">
+                <img class="contact-image mb-3" src="../../assets/img/mail.png" alt="mail apoe4">
                 <h5 class="contact-heading mt-5">SAY HELLO</h5>
+                <div class="link">
                 <a href="mailto:apoe4@hng.com" class="contact-link">apoe4@hng.com</a>
+                <br/>
                 <a href="mailto:hello@apoe4.com" class="contact-link">hello@apoe4.com</a>
+                </div>
+                
             </div>
             <div class="col-md-3"></div>
        </div>
@@ -43,13 +51,17 @@
 
         }
         .contact .address{
-            font-size:10px;
+            font-size:13px;
+            color:black;
+            margin-top:-10px;
+
 
         }
         .contact .contact-heading{
             font-weight: bold;
             margin-bottom: 20px;
-            font-size: 10px;
+            font-size: 16px;
+
         }
 
         .contact .icon {
@@ -59,6 +71,16 @@
 
         .contact .contact-link{
             text-transform: lowercase;
+            font-size: 14px;
+        }
+
+        .contact a {
+            font-weight: 200;
+        }
+
+        .contact .link{
+            margin-top:-10px;
+
         }
         
        
