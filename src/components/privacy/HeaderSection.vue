f3b252960ea729098c18929bc0e81ed6b3855806
Ahmzyjazzy <olanrewajuahmed095@yahoo.com>
2019-04-19 05:17:31 +0100

    feat/add header section for about and privacy page

--- src/components/privacy/HeaderSection.vue
+++ src/components/privacy/HeaderSection.vue
@@ -1,12 +1,26 @@
 <template>
-    <div class="container-fluid">
+    <div class="container-fluid" id="privacy" :style="{ backgroundImage: 'url(' + require('@/assets/img/r29.png') + ')' }">
         <div class="container">
-            <!-- ALL COMPONENT MARKUP GOES HERE-->
+            <h1 class="font-weight-bold text-white">Privacy</h1>
+            <p class="text-white">This is a sample content privacy us. This is a sample content privacy us</p>
         </div>
     </div>
 </template>
 
 <style scoped>
+
+    #privacy {
+        background-repeat: no-repeat;
+        background-position: right;
+    }
+
+    #privacy .container {
+        height: 300px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: left;
+    }
 
 </style>
 
