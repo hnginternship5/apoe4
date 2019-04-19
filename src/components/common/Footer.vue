5ae87fc770e46d2706bfdf4018fb9acbd8e04277
Monday <mundial419@gmail.com>
2019-04-19 04:14:14 +0100

    Addes JS code for scroll to top functionality

--- src/components/common/Footer.vue
+++ src/components/common/Footer.vue
@@ -129,11 +129,32 @@
     computed: {
       ...mapGetters(['links'])
     },
-    methods: {
+    methods: { 
+	
       onClick (e, item) {
         e.stopPropagation()
         if (item.to || !item.href) return
       }
+window.onscroll = function(){
+scrollUp();};
+
+//create scroll function, if user scrolls 40px from top, scroll button becomes visible
+
+function scrollUp(){
+	if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100){
+		document.getElementById("scrollBut").style.display = "block";
+	}
+	else {
+		document.getElementById("scrollBut").style.display = "none";
+	}
+};
+//create function which takes user to the top upon clicking
+
+function scrollToTop(){
+	document.body.scrollTop =  0;
+	document.documentElement.scrollTop = 0;
+}
     }
+	
   }
 </script>
