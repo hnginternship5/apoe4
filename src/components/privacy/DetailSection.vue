a885a01e2ef64f437d066475970d8badd4e22dec
Edmund <edmund.timfon@gmail.com>
2019-04-29 17:16:35 +0100

    fix privacy page

--- src/components/privacy/DetailSection.vue
+++ src/components/privacy/DetailSection.vue
@@ -1,39 +1,68 @@
 <template>
   <div class="container-fluid">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <h2>Privacy</h2>
-            <p>Protecting your privacy is important to us. Accordingly, we’re providing this Privacy Policy to explain our practices regarding t
-              he collection, use and disclosure of information that we receive when you use our Services.
-              This Privacy Policy applies only to those websites, services and applications included within “Services” and doesn’t apply to any third-party websites, 
-              services or applications, even if they are accessible through our Services. Also, please note that, unless we define a term in this Privacy Policy, 
-              all capitalized words used in this Privacy Policy have the same meanings as in our Terms of Service.</p>
-          </div>
+    <div class="container">
+      <div class="row main-content">
+        <div class="col-md-12 mb-5 mt-5">
+          <h3>Privacy</h3>
+          <p class="mt-4 details">
+            Protecting your privacy is important to us. Accordingly, we’re providing this Privacy Policy to explain our practices regarding the collection, use and disclosure of information that we receive when you use our Services.
+            This Privacy Policy applies only to those websites, services and applications included within “Services” and doesn’t apply to any third-party websites,
+            services or applications, even if they are accessible through our Services. Also, please note that, unless we define a term in this Privacy Policy,
+            all capitalized words used in this Privacy Policy have the same meanings as in our Terms of Service.
+          </p>
+        </div>
 
-          <div class="col-md-12">
-            <h2>APOE4 Copyright & Intellectual Property Policy</h2>
-            <p>APOE4 respects the intellectual property rights of others and expects its users to do the same. It is APOE4’s policy,
-              in appropriate circumstances and at its discretion, to disable and/or terminate the accounts of users who repeatedly infringe or are repeatedly 
-              charged with infringing the copyrights or other intellectual property rights of others. In accordance with the Digital Millennium Copyright Act of 1998, 
-              the text of which may be found on the U.S. Copyright Office website at http://www.copyright.gov/legislation/dmca.pdf, APOE4 will respond expeditiously to
-              claims of copyright infringement committed 
-              using the APOE4 website (the “Site”) that are reported to APOE4’s Designated Copyright Agent, identified in the sample notice below.</p>
-          </div>
-          <div class="col-md-12">
-            <h2>Sub Processors</h2>
-            <p>To support delivery of our Services, APOE4 may engage and use data processors with access to certain Customer Data (each, a "Sub-processor"). This page provides important information about the identity, location and role of each Sub-processor.</p>
-          </div>
+        <div class="col-md-12 mb-5">
+          <h3>APOE4 Copyright &amp; Intellectual Property Policy</h3>
+          <p class="mt-4 details">
+            APOE4 respects the intellectual property rights of others and expects its users to do the same. It is APOE4’s policy,
+            in appropriate circumstances and at its discretion, to disable and/or terminate the accounts of users who repeatedly infringe or are repeatedly
+            charged with infringing the copyrights or other intellectual property rights of others. In accordance with the Digital Millennium Copyright Act of 1998,
+            the text of which may be found on the U.S. Copyright Office website at
+            <em
+              class="link"
+            >
+              <a
+                :href="'http://www.copyright.gov/legislation/dmca.pdf'"
+                :target="'_blank'"
+              >www.copyright.gov/legislation/dmca</a>
+            </em>, APOE4 will respond expeditiously to
+            claims of copyright infringement committed
+            using the APOE4 website (the “Site”) that are reported to APOE4’s Designated Copyright Agent, identified in the sample notice below.
+          </p>
+        </div>
+        <div class="col-md-12 mb-5">
+          <h3>Sub Processors</h3>
+          <p
+            class="mt-4 details"
+          >To support delivery of our Services, APOE4 may engage and use data processors with access to certain Customer Data (each, a "Sub-processor"). This page provides important information about the identity, location and role of each Sub-processor.</p>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-  .row {
-    margin-bottom: 30px;
-  }
-
+.main-content {
+  font-family: Roboto, 'Poppins', sans-serif;
+}
+.row {
+  margin-bottom: 30px;
+}
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-weight: bold;
+}
+.details {
+  line-height: 1.7rem;
+}
+.link {
+  border-bottom: 1px solid var(--mainBlack);
+}
 </style>
 
 <script>
